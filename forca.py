5e04585132c5b5790bfa3a325c965e8560149012
Fabio Dias <fabio.dias@mvisia.com.br>
2022-05-22 23:10:09 -0300

    Addition of game shutdown and improvements in code modularization.

--- forca.py
+++ forca.py
@@ -1,30 +1,85 @@
-def jogar():
-    print("*********************************")
-    print("***Bem vindo no jogo de Forca!***")
-    print("*********************************")
+import random
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
+def jogar():
+
+    imprime_mensagem_abertura()
+    palavra_secreta = carrega_palavra_secreta()
+
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+    print(letras_acertadas)
 
     enforcou = False
     acertou = False
-
-    print(letras_acertadas)
+    erros = 0
 
     while not enforcou and not acertou:
 
-        chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = pede_chute()
 
-        index = 0
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                letras_acertadas[index] = letra
-            index = index + 1
+        if chute in palavra_secreta:
+            marca_chute_correto(chute, letras_acertadas, palavra_secreta)
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
 
         print(letras_acertadas)
 
-    print("Fim do jogo")
+    if acertou:
+        imprime_mensagem_vencedor()
+    else:
+        imprime_mensagem_perdedor()
+
+
+def imprime_mensagem_abertura():
+    print("*********************************")
+    print("***Bem vindo no jogo de Forca!***")
+    print("*********************************")
+    
+
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    
+    return palavra_secreta
+
+
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
+
+
+def pede_chute():
+    chute = input("Qual letra? ")
+    chute = chute.strip().upper()
+    return chute
+
+
+def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if chute == letra:
+            letras_acertadas[index] = letra
+        index += 1
+
+
+def imprime_mensagem_vencedor():
+    print("Você ganhou!!")
+
+
+def imprime_mensagem_perdedor():
+    print("Você perdeu!!")
+
 
 if __name__ == "__main__":
     jogar()
